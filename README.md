5f3799020959be059e3303081a65da1503527175
sugoidogo <sugoidogo@sugoidogo.com>
2025-01-27 10:23:54 +0000

    initial release

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cloudflare R2 Browser
+
+[![Deploy to Cloudflare Workers](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/sugoidogo/cf-r2-browser)
+
+This worker allows you to browse and download files from R2 buckets in your web browser.
+Supports multiple domains and buckets from a single worker.
+Suppors configuration via `wrangler.toml` or Cloudflare dashboard.
+
+# Setup
+Deploy with the button above, or clone this repo and configure via `wrangler.toml` before deploying.
+In your worker settings, you must set a custom domain (unless you want to use the default workers subdomain), bind an r2 bucket, and then create a variable binding where the key is the domain name and the value is the variable name of the bucket binding. If you don't understand what that means, check the comments in `wrangler.toml`
--- src/index.js
+++ src/index.js
@@ -1,15 +1,64 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+	async fetch(request, env) {
+		const url = new URL(request.url)
+		const bucketName = env[url.hostname]
+		const bucket = env[bucketName]
+		const objectName = url.pathname.substring(1)
+		if (request.method === 'GET') {
+			if (!objectName || objectName.endsWith('/')) {
+				const options = { prefix: objectName }
+				let listing = null
+				const list = new Set()
+				do {
+					listing = await bucket.list(options)
+					options.cursor = listing.cursor
+					for (const object of listing.objects) {
+						/** @type {String} */
+						let itemName = object.key.slice(objectName.length)
+						itemName = itemName.substring(itemName.indexOf('/') + 1)
+						list.add(itemName)
+					}
+				} while (listing.truncated)
+				if (list.size === 0) {
+					return new Response(null, { status: 404 })
+				}
+				let body = '<h1>listing of /' + objectName + '</h1><br>'
+				for (const itemName of list) {
+					body += '<a href=' + itemName + '>' + itemName + '</a>'
+				}
+				return new Response(body, { headers: { 'content-type': 'text/html' } })
+			}
+
+			const object = await bucket.get(objectName, {
+				range: request.headers,
+				onlyIf: request.headers,
+			})
+
+			if (object === null) {
+				return new Response(null, { status: 404 })
+			}
+
+			const headers = new Headers()
+			object.writeHttpMetadata(headers)
+			headers.set('etag', object.httpEtag)
+			if (object.range) {
+				headers.set("content-range", `bytes ${object.range.offset}-${object.range.end ?? object.size - 1}/${object.size}`)
+			}
+			const status = object.body ? (request.headers.get("range") !== null ? 206 : 200) : 304
+			return new Response(object.body, { status: status, headers: headers })
+		}
+
+		if (request.method === 'HEAD') {
+			const object = await bucket.head(objectName)
+
+			if (object === null) {
+				return new Response(null, { status: 404 })
+			}
+
+			const headers = new Headers()
+			object.writeHttpMetadata(headers)
+			headers.set('etag', object.httpEtag)
+			return new Response(null, { headers: headers })
+		}
+	}
+}
